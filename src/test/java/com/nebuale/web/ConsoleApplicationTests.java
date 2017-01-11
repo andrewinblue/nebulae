1344af564696441bd6d2918334f553c10995b9e2
andrewinblue <andrewchen@Andrews-iMac.domain>
2017-01-10 21:12:23 -0800

    andrche changed test message

--- src/test/java/com/nebuale/web/ConsoleApplicationTests.java
+++ src/test/java/com/nebuale/web/ConsoleApplicationTests.java
@@ -11,7 +11,7 @@ public class ConsoleApplicationTests {
 
 	@Test
 	public void contextLoads() {
-	   System.out.println("Test Message 01");
+	   System.out.println("Test Message 04");
 	}
 
 	@Test
